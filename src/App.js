86e9f618d568c49e2f564b51c4451bf7f14aa25d
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2022-10-19 00:03:15 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import crosswordia from "./img/crosswordia_gaus.png"
+// import Button from "react-bootstrap/Button"
+import { Navbar, Container, Nav, NavDropdown, Row, Col, Image, Button, Card } from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
+      <main>
+        {/* <img src="{crosswordia}" alt="" /> */}
+        <Container>
+          <Row className="px-4 my-5">
+            <Col sm={7}>
+              <Image src="https://picsum.photos/900/400" fluid rounded className="" />
+              {/* <Image src= fluid rounded className="" referrerpolicy="no-referrer" /> */}
+            </Col>
+            <Col sm={5}>
+              <h1 class="font-weight-light">Tagline</h1>
+              {/* <h1 class="">Tagline</h1> */}
+              <p class="mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat ipsa aliquam, distinctio facilis error consequatur corporis vel recusandae necessitatibus aliquid, quibusdam modi velit voluptatum animi laborum nulla nostrum rerum quidem!</p>
+              <Button variant="outline-primary">Call to Action</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Card className="text-center bg-secondary text-white my-5 py-4">
+              <Card.Body>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Body>
+            </Card>
+          </Row>
+          <Row>
+            <Col>
+              <Card className="h-100" style={{ width: "18rem" }}>
+                {/* <Card.Img variant="top" src="https://picsum.photos/id/201/320/200" /> */}
+                <Card.Img variant="top" src={crosswordia} />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                <Card.Footer>
+                  {/* <small className="text-muted">https://crosswordia.app/</small> */}
+                  <small className="text-muted">
+                    <a href="https://crosswordia.app/">https://crosswordia.app/</a>
+                  </small>
+                  {/* <div className="text-muted">https://crosswordia.app/</div> */}
+                </Card.Footer>
+              </Card>
+            </Col>
+            <Col>
+              <Card className="h-100" style={{ width: "18rem" }}>
+                <Card.Img variant="top" src="https://picsum.photos/320/200" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Placeholder</small>
+                </Card.Footer>
+              </Card>
+            </Col>
+            <Col>
+              <Card className="h-100" style={{ width: "18rem" }}>
+                <Card.Img variant="top" src="https://picsum.photos/id/200/320/200" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Placeholder</small>
+                </Card.Footer>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+      <footer class="py-5 my-5 bg-dark">
+        <Container className="px-4">
+          <p class="text-center text-white">Copyright &copy; {new Date().getFullYear()}</p>
+        </Container>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
